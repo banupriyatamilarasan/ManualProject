edefff4de9b3a8f1e01b0c50babff7d3a75104b3
banupriyatamilarasan <banupriyasr90@gmail.com>
2024-09-14 20:10:01 +0530

    credit

---
+++ src/main/java/org/com/Testing.java
@@ -0,0 +1,17 @@
+package org.com;
+
+public class Testing {
+	private void stu1() {
+		System.out.println("t");
+		
+	}
+	private void stu2() {
+	System.out.println("l");
+		
+	}
+	private void stu3() {
+		System.out.println("m");
+		
+	}
+
+}
